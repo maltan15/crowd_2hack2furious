d88e1282d98cf199cd70f1b7e005bea2b21ff727
KevSornet <kevin.sornet@obf.ateneo.edu>
2020-11-28 22:26:00 +0800

    added stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(Crowd());
+}
+
+class Crowd extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Crowd'),
+        ),
+      ),
+    );
+  }
+}
